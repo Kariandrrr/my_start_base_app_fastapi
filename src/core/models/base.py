4c19adb59e606298ca604e1acb34a1fd2ce22dfb
Suslova Karina <suslovakarina32@gmail.com>
2026-01-12 22:31:03 +0300

    new naming convention db

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,11 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy.orm import Mapped, mapped_column
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming.convention)
 
     @declared_attr
     def __tablename__(cls) -> str:
