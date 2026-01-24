8069fcb173309480cfdefa9dceeb3b53c3a4a362
Kariandrrr <suslovakarina32@gmail.com>
2026-01-24 21:31:53 +0300

    Update env.py

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -1,5 +1,6 @@
 import asyncio
 from logging.config import fileConfig
+from pathlib import Path
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
@@ -7,8 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from ..core.config import settings
-from ..core.models import Base
+from core.config import settings
+from core.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,12 +26,21 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 config.set_main_option("sqlalchemy.url", str(settings.db.url))
+
+
+ALEMBIC_DIR = Path(__file__).resolve().parent
+FUNCTIONS_DIR = ALEMBIC_DIR / "functions"
+
+config.set_section_option(
+    "extra",
+    "functions.dir",
+    str(FUNCTIONS_DIR),
+)
 
 
 def run_migrations_offline() -> None:
